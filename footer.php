cd94eb8b35b765da61f88221a6d3fe2504ce6ec0
Benjamin Intal <bfintal@gmail.com>
2019-02-28 16:37:52 +0800

    Post shouldn't ever be dirty

--- footer.php
+++ footer.php
@@ -1,3 +1,19 @@
+<script>
+function ready(fn) {
+  if (document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading"){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+ready( function() {
+    setTimeout( function() {
+        window.wp.data.select( 'core/editor' ).isEditedPostDirty = function() { 
+            return false;
+        }
+    }, 1 )
+} )
+</script>
 <?php
 
 wp_footer();
